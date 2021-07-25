5fb936363c4d1bca0a779088925b145bce6c7021
yongyuanlin <76577177+yongyuanlin@users.noreply.github.com>
2021-07-25 10:03:12 +0800

    Update jx_cfd.js

--- jx_cfd.js
+++ jx_cfd.js
@@ -26,7 +26,7 @@ $.innerInviteList = [];
 const HelpAuthorFlag = true;//是否助力作者SH  true 助力，false 不助力
 
 // 热气球接客 每次运行接客次数
-let serviceNum = 1;// 每次运行接客次数
+let serviceNum = 5;// 每次运行接客次数
 if ($.isNode() && process.env.gua_wealth_island_serviceNum) {
   serviceNum = Number(process.env.gua_wealth_island_serviceNum);
 }
