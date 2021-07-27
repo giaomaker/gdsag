23a39ad1dd18a1a312373a3dd7fa7f64ac42d5f5
yongyuanlin <76577177+yongyuanlin@users.noreply.github.com>
2021-07-27 19:26:28 +0800

    更新

--- jd_necklace.js
+++ jd_necklace.js
@@ -7,7 +7,6 @@ Last Modified time: 2021-05-28 17:27:14
 [task_local]
 #点点券
 20 0,20 * * * jd_necklace.js, tag=点点券, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
-
  */
 const $ = new Env('点点券');
 const ZooFaker=require('./ZooFaker_Necklace.js').utils;
@@ -74,18 +73,18 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
     })
 async function jd_necklace() {
   try {
-     await necklace_homePage();
-     await $.wait(2000)
-     await doTask();
-     await $.wait(2000)
-     await sign();
-     await $.wait(2000)
-     await necklace_homePage();
-     await receiveBubbles();
-     await necklace_homePage();
-     // // await necklace_exchangeGift($.totalScore);//自动兑换多少钱的无门槛红包，1000代表1元，默认兑换全部点点券
-     await showMsg();
-     await $.wait(2000)
+    await necklace_homePage();
+    await $.wait(2000)
+    await doTask();
+    await $.wait(2000)
+    await sign();
+    await $.wait(2000)
+    await necklace_homePage();
+    await receiveBubbles();
+    await necklace_homePage();
+    // // await necklace_exchangeGift($.totalScore);//自动兑换多少钱的无门槛红包，1000代表1元，默认兑换全部点点券
+    await showMsg();
+    await $.wait(2000)
   } catch (e) {
     $.logErr(e)
   }
@@ -418,18 +417,18 @@ function getCcTaskList(functionId, body, type = '3') {
   let url = `https://api.m.jd.com/client.action?functionId=${functionId}`
   return new Promise(resolve => {
     if (functionId === 'getCcTaskList') {
-      
+
     }
     if (functionId === 'reportCcTask'){
-      
+
     }
     if (functionId === 'reportSinkTask'){
       url += body
       body = ''
     }
     // if (type === '4' && functionId === 'reportCcTask'){
-      // url = `https://api.m.jd.com/client.action?functionId=${functionId}&body=${escape(JSON.stringify(body))}&uuid=8888888&client=apple&clientVersion=9.4.1&st=1622193986049&sign=f5abd9fd7b9b8abaa25b34088f9e8a54&sv=102`
-      // body = `body=${escape(JSON.stringify(body))}`
+    // url = `https://api.m.jd.com/client.action?functionId=${functionId}&body=${escape(JSON.stringify(body))}&uuid=8888888&client=apple&clientVersion=9.4.1&st=1622193986049&sign=f5abd9fd7b9b8abaa25b34088f9e8a54&sv=102`
+    // body = `body=${escape(JSON.stringify(body))}`
     // }
     const options = {
       url,
