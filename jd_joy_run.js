abcb1394012f30b43be18ba4bf5c9ab2452d2d7d
yongyuanlin <76577177+yongyuanlin@users.noreply.github.com>
2021-07-29 11:22:58 +0800

    修复宠汪汪

--- jd_joy_run.js
+++ jd_joy_run.js
@@ -9,18 +9,14 @@ token获取途径：
 1、微信搜索'来客有礼'小程序,登陆京东账号，点击底部的'我的'或者'发现'两处地方,即可获取Token，脚本运行提示token失效后，继续按此方法获取即可
 2、或者每天去'来客有礼'小程序->宠汪汪里面，领狗粮->签到领京豆 也可获取Token(此方法每天只能获取一次)
 脚本里面有内置提供的friendPin，如果你没有修改脚本或者BoxJs处填写自己的互助码，会默认给脚本内置的助力。
-
 docker 设置环境变量 JOY_RUN_HELP_MYSELF 为true,则开启账号内部互助.默认关闭(即给脚本作者内置的助力).
-
 [MITM]
 hostname = draw.jdfcloud.com
-
 ===========Surge=================
 [Script]
 宠汪汪邀请助力与赛跑助力 = type=cron,cronexp="15 10 * * *",wake-system=1,timeout=3600,script-path=jd_joy_run.js
 宠汪汪助力更新Token = type=http-response,pattern=^https:\/\/draw\.jdfcloud\.com(\/mirror)?\/\/api\/user\/addUser\?code=, requires-body=1, max-size=0, script-path=jd_joy_run.js
 宠汪汪助力获取Token = type=http-request,pattern=^https:\/\/draw\.jdfcloud\.com(\/mirror)?\/\/api\/user\/user\/detail\?openId=, max-size=0, script-path=jd_joy_run.js
-
 ===================Quantumult X=====================
 [task_local]
 # 宠汪汪邀请助力与赛跑助力
@@ -30,7 +26,6 @@ hostname = draw.jdfcloud.com
 ^https:\/\/draw\.jdfcloud\.com(\/mirror)?\/\/api\/user\/addUser\?code= url script-response-body jd_joy_run.js
 # 宠汪汪助力获取Token
 ^https:\/\/draw\.jdfcloud\.com(\/mirror)?\/\/api\/user\/user\/detail\?openId= url script-request-header jd_joy_run.js
-
 =====================Loon=====================
 [Script]
 cron "15 10 * * *" script-path=jd_joy_run.js, tag=宠汪汪邀请助力与赛跑助力
@@ -50,12 +45,16 @@ const JD_BASE_API = `https://draw.jdfcloud.com//pet`;
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : {};
 //下面给出好友邀请助力的示例填写规则
-let invite_pins = ['weiguoqw,122827561-122864,13535330772_p,jd_5e6adefd4d2c0,jd_77c410a580577,jd_54a04ef64c7f2'];
+let invite_pins = [];
 //下面给出好友赛跑助力的示例填写规则
-let run_pins = ['weiguoqw,122827561-122864,13535330772_p,jd_5e6adefd4d2c0,jd_77c410a580577,jd_54a04ef64c7f2'];
+//"zhaosen2580", "jd_47ee22449e303", "jd_6c5e39478ec3b", "jd_4346918b58d6e", "liuz9988", "88489948", "jd_61f1269fd3236"
+let run_pins = [];
 //friendsArr内置太多会导致IOS端部分软件重启,可PR过来(此处目的:帮别人助力可得30g狗粮)
-let friendsArr = ["weiguoqw", "wdNMTUDYYGaLoaC", "122827561-122864", "13535330772_p", "jd_5e6adefd4d2c0", "jd_77c410a580577", "jd_54a04ef64c7f2"]
-
+let friendsArr = []
+const JOY_RUN_HELP_MYSELF = true;// 每次运行接客次数
+if ($.isNode() && process.env.JOY_RUN_HELP_MYSELF) {
+  JOY_RUN_HELP_MYSELF = process.env.gua_wealth_island_serviceNum;
+}
 
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
@@ -133,7 +132,7 @@ async function main() {
       // const zooFaker = require('./utils/JDJRValidator_Pure');
       // $.validate = await zooFaker.injectToRequest()
       if ($.isNode()) {
-        if (process.env.JOY_RUN_HELP_MYSELF) {
+        if (JOY_RUN_HELP_MYSELF) {
           console.log(`\n赛跑会先给账号内部助力,如您当前账户有剩下助力机会则为lx0301作者助力\n`)
           let my_run_pins = [];
           Object.values(jdCookieNode).filter(item => item.match(/pt_pin=([^; ]+)(?=;?)/)).map(item => my_run_pins.push(decodeURIComponent(item.match(/pt_pin=([^; ]+)(?=;?)/)[1])))
@@ -145,8 +144,8 @@ async function main() {
           run_pins = run_pins[0].split(',')
           Object.values(jdCookieNode).filter(item => item.match(/pt_pin=([^; ]+)(?=;?)/)).map(item => run_pins.push(decodeURIComponent(item.match(/pt_pin=([^; ]+)(?=;?)/)[1])))
           run_pins = [...new Set(run_pins)];
-          let fixPins = run_pins.splice(run_pins.indexOf('被折叠的记忆33'), 1);
-          fixPins.push(...run_pins.splice(run_pins.indexOf('jd_6cd93e613b0e5'), 1));
+          let fixPins = run_pins.splice(run_pins.indexOf('18237091240_p'), 1);
+          fixPins.push(...run_pins.splice(run_pins.indexOf('pluto.5218_m'), 1));
           const randomPins = getRandomArrayElements(run_pins, run_pins.length);
           run_pins = [[...fixPins, ...randomPins].join(',')];
           invite_pins = run_pins;
